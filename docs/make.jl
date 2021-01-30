9f19c8baab7e7b11c86a59fb7e2f17a6cbb4a82d
Hongyang Zhou <hyzhou@umich.edu>
2021-01-30 12:54:43 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using TestParticle
+using Documenter
+
+makedocs(;
+    modules=[TestParticle],
+    authors="Hongyang Zhou <hyzhou@umich.edu> and contributors",
+    repo="https://github.com/henry2004y/TestParticle.jl/blob/{commit}{path}#L{line}",
+    sitename="TestParticle.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://henry2004y.github.io/TestParticle.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/henry2004y/TestParticle.jl",
+)
