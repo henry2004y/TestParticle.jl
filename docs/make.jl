a8b9cfbeb510b4cab5f92e7999abf8c3543be217
Hongyang Zhou <hyzhou@umich.edu>
2021-02-03 16:00:51 +0200

    First version

--- docs/make.jl
+++ docs/make.jl
@@ -13,6 +13,8 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Example" => "example.md",
+        "Internal" => "internal.md",
     ],
 )
 
---
+++ src/constants.jl
@@ -0,0 +1,8 @@
+# Physical constants in SI units
+const qₑ = -1.60217662e-19  # electron charge, [C]
+const mₑ = 9.10938356e-31   # electron mass, [kg]
+const qᵢ = 1.60217662e-19   # proton mass, [C]
+const mᵢ = 1.673557546e-27  # proton mass, [kg]
+const c  = 3e8              # speed of light, [m/s]
+const μ₀ = 4π*1e-7          # Vacuum permeability, [H/m]
+const Rₑ = 6.38e6           # Earth radius, [m]
