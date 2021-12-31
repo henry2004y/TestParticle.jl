d2967fd847dcaafd4249526e598324c8b317bf90
Hongyang Zhou <hyzhou@umich.edu>
2021-12-31 14:11:17 +0200

    Use internal utility func

--- examples/demo_batsrus_3dstructured.jl
+++ examples/demo_batsrus_3dstructured.jl
@@ -23,28 +23,6 @@ function prob_func(prob, i, repeat)
    remake(prob,u0=initial_conditions(i))
 end
 
-"""
-    set_axes_equal(ax)
-
-Set 3D plot axes to equal scale.
-Make axes of 3D plot have equal scale so that spheres appear as spheres and
-cubes as cubes. Required since `ax.axis('equal')` and `ax.set_aspect('equal')`
-don't work on 3D.
-"""
-function set_axes_equal(ax)
-   limits = zeros(2,3)
-   limits[:,1] .= ax.get_xlim3d()
-   limits[:,2] .= ax.get_ylim3d()
-   limits[:,3] .= ax.get_zlim3d()
-   origin = mean(limits, dims=1)
-   radius = @. 0.5 * max(abs(limits[2,:] - limits[1,:]))
-
-   x, y, z = origin
-   ax.set_xlim3d([x - radius[1], x + radius[1]])
-   ax.set_ylim3d([y - radius[2], y + radius[2]])
-   ax.set_zlim3d([z - radius[3], z + radius[3]])
-end
-
 ## Data processing
 
 filename = "3d_var_region0_0_t00002500_n00106468.out"
@@ -161,6 +139,6 @@ ylabel("y [Rg]")
 zlabel("z [Rg]")
 
 ax.set_box_aspect([1.17,4,4])
-set_axes_equal(ax)
+TestParticle.set_axes_equal(ax)
 
 #plotdata(data, "bz", plotmode="contbar", cut="y", cutPlaneIndex=40, level=20)
