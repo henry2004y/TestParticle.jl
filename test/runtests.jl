4eccb4dc75635e3543a676f5989d6eb01c6c3532
TCLiuu <liutc@mail.nankai.edu.cn>
2022-07-08 11:51:25 +0000

    test: add a new test set about mixed fields

--- test/runtests.jl
+++ test/runtests.jl
@@ -108,4 +108,39 @@ end
       @test x[306] ≈ 1.2588844644203672e7 rtol=1e-6
 
    end
+
+   @testset "mixed fields" begin
+      x = range(-10, 10, length=15)
+      y = range(-10, 10, length=20)
+      z = range(-10, 10, length=25)
+      B = fill(0.0, 3, length(x), length(y), length(z)) # [T]
+
+      B[3,:,:,:] .= 10e-9
+      E_field(r) = SA[0, 0, 5e-10]
+
+      Δx = x[2] - x[1]
+      Δy = y[2] - y[1]
+      Δz = z[2] - z[1]
+
+      mesh = CartesianGrid((length(x)-1, length(y)-1, length(z)-1),
+         (x[1], y[1], z[1]),
+         (Δx, Δy, Δz))
+
+      x0 = [0.0, 0.0, 0.0] # initial position, [m]
+      u0 = [1.0, 0.0, 0.0] # initial velocity, [m/s]
+      stateinit = [x0..., u0...]
+
+      param = prepare(mesh, E_field, B)
+      tspan = (0.0, 1.0)
+
+      prob = ODEProblem(trace!, stateinit, tspan, param)
+
+      sol = solve(prob, Tsit5(); save_idxs=[1,2,3])
+
+      x = getindex.(sol.u, 1)
+      y = getindex.(sol.u, 2)
+      z = getindex.(sol.u, 3)
+
+      @test length(x) == 8 && x[end] ≈ 0.8540967226885379
+   end
 end
