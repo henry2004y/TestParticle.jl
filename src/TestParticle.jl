b7e5d0fabc1060754f56d86de5a01eb9068abb97
TCLiuu <liutc@mail.nankai.edu.cn>
2022-07-16 10:30:33 +0000

    feat: add the support for time-dependent fields

--- src/TestParticle.jl
+++ src/TestParticle.jl
@@ -62,8 +62,37 @@ function getinterp(A, gridx, gridy, gridz)
       return SA[interpx(r...), interpy(r...), interpz(r...)]
    end
 
-   return get_field
+   return Field(get_field)
 end
+
+function is_time_dependent(f)
+   itp_param_number = 2
+   numargs = [length(m.sig.parameters)-1 for m in methods(f)]
+   itp = any(x -> x == itp_param_number, numargs)
+   if !itp
+      if any(x -> x == itp_param_number-1, numargs)
+         return false
+      else
+         name = nameof(f)
+         throw(ArgumentError("All methods for the field function $name had too many arguments."))
+      end
+   else
+      return true
+   end
+end
+
+abstract type AbstractField{itd} end
+
+struct Field{itd, F} <: AbstractField{itd}
+   field_function::F
+end
+
+Field(f::Function) = Field{is_time_dependent(f), typeof(f)}(f)
+
+(f::AbstractField{true})(xu, t) = f.field_function(xu, t)
+(f::AbstractField{true})(xu) = throw(ArgumentError("Time-dependent field function must have a time argument."))
+(f::AbstractField{false})(xu, t) = f.field_function(xu)
+(f::AbstractField{false})(xu) = f.field_function(xu)
 
 """
     prepare(grid, E, B; species=Proton) -> (q, m, E, B)
@@ -76,8 +105,8 @@ function prepare(grid::CartesianGrid, E::TE, B::TB; species::Species=Proton, q=1
 
    gridx, gridy, gridz = makegrid(grid)
 
-   E = TE <: AbstractArray ? getinterp(E, gridx, gridy, gridz) : E
-   B = TB <: AbstractArray ? getinterp(B, gridx, gridy, gridz) : B
+   E = TE <: AbstractArray ? getinterp(E, gridx, gridy, gridz) : Field(E)
+   B = TB <: AbstractArray ? getinterp(B, gridx, gridy, gridz) : Field(B)
 
    q, m, E, B
 end
@@ -93,9 +122,9 @@ function prepare(grid::CartesianGrid, E::TE, B::TB, F::TF; species::Species=Prot
 
    gridx, gridy, gridz = makegrid(grid)
 
-   E = TE <: AbstractArray ? getinterp(E, gridx, gridy, gridz) : E
-   B = TB <: AbstractArray ? getinterp(B, gridx, gridy, gridz) : B
-   F = TF <: AbstractArray ? getinterp(F, gridx, gridy, gridz) : F
+   E = TE <: AbstractArray ? getinterp(E, gridx, gridy, gridz) : Field(E)
+   B = TB <: AbstractArray ? getinterp(B, gridx, gridy, gridz) : Field(B)
+   F = TF <: AbstractArray ? getinterp(F, gridx, gridy, gridz) : Field(F)
 
    q, m, E, B, F
 end
@@ -109,6 +138,8 @@ and mass `m` and analytic EM field functions. Prescribed `species` are `Electron
 """
 function prepare(E, B; species::Species=Proton, q=1.0, m=1.0)
    q, m = getchargemass(species, q, m)
+   B = Field(B)
+   E = Field(E)
 
    q, m, E, B
 end
@@ -121,7 +152,7 @@ and mass `m`, analytic EM field functions, and external force `F`.
 """
 function prepare(E, B, F; species::Species=Proton, q=1.0, m=1.0)
    t = prepare(E, B; species, q, m)
-   push!(t, F)
+   push!(t, Field(F))
    t
 end
 
@@ -129,13 +160,13 @@ end
 function trace!(dy, y, p, t)
    q, m, E, B = p
    dy[1:3] = y[4:6]
-   dy[4:6] = q/m*(E(y) + y[4:6] × (B(y[1:3])))
+   dy[4:6] = q/m*(E(y, t) + y[4:6] × (B(y, t)))
 end
 
 function trace(y, p, t)
    q, m, E, B = p
    dx, dy, dz = y[4:6]
-   dux, duy, duz = q/m*(E(y) + y[4:6] × (B(y[1:3])))
+   dux, duy, duz = q/m*(E(y, t) + y[4:6] × (B(y, t)))
    SVector{6}(dx, dy, dz, dux, duy, duz)
 end
 
@@ -144,13 +175,13 @@ field."
 function trace_full!(dy, y, p, t)
    q, m, E, B, F = p
    dy[1:3] = y[4:6]
-   dy[4:6] = (q*(E(y) + y[4:6] × (B(y[1:3]))) + F(y)) / m
+   dy[4:6] = (q*(E(y, t) + y[4:6] × (B(y, t))) + F(y, t)) / m
 end
 
 function trace_full(y, p, t)
    q, m, E, B, F = p
    dx, dy, dz = y[4:6]
-   dux, duy, duz = (q*(E(y) + y[4:6] × (B(y[1:3]))) + F(y)) / m
+   dux, duy, duz = (q*(E(y, t) + y[4:6] × (B(y, t))) + F(y, t)) / m
    SVector{6}(dx, dy, dz, dux, duy, duz)
 end
 
@@ -163,7 +194,7 @@ function trace_relativistic!(dy, y, p, t)
    end
    γInv = √(1.0 - (y[4]*y[4] + y[5]*y[5] + y[6]*y[6])/c^2)
    dy[1:3] = y[4:6]
-   dy[4:6] = q/m*γInv*(E(y) + y[4:6] × (B(y[1:3])))
+   dy[4:6] = q/m*γInv*(E(y, t) + y[4:6] × (B(y, t)))
 end
 
 function trace_relativistic(y, p, t)
@@ -174,7 +205,7 @@ function trace_relativistic(y, p, t)
    end
    γInv = √(1.0 - (y[4]*y[4] + y[5]*y[5] + y[6]*y[6])/c^2)
    dx, dy, dz = y[4:6]
-   dux, duy, duz = q/m*γInv*(E(y) + y[4:6] × (B(y[1:3])))
+   dux, duy, duz = q/m*γInv*(E(y, t) + y[4:6] × (B(y, t)))
    SVector{6}(dx, dy, dz, dux, duy, duz)
 end
 end
