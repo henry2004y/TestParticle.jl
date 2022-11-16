e8e14d0006c2d15c4c945c4b4596947c53414b76
Hongyang Zhou <hyzhou@umich.edu>
2022-11-16 16:57:55 +0200

    Out of domain (#54)
    * Format code
    
    * Add isoutofdomain

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,15 @@ function prob_func(prob, i, repeat)
    remake(prob, u0=rand(MersenneTwister(i))*prob.u0)
 end
 
+"Test boundary check method."
+function isoutofdomain(u, p, t)
+   if hypot(u[1], u[2], u[3]) > 0.8
+      return true
+   else
+      return false
+   end
+end
+
 @testset "TestParticle.jl" begin
    @testset "numerical field" begin
       x = range(-10, 10, length=15)
@@ -33,11 +42,11 @@ end
 
       param = prepare(x, y, z, E, B)
       prob = ODEProblem(trace!, stateinit, tspan, param)
-      sol = solve(prob, Tsit5(); save_idxs=[1])
+      sol = solve(prob, Tsit5(); save_idxs=[1], isoutofdomain, verbose=false)
 
       x = getindex.(sol.u, 1)
-
-      @test length(x) == 8 && x[end] ≈ 0.8540967226885379
+      # There are numerical differences on x86 and ARM platforms!
+      @test x[end] ≈ 0.7388945226814018
 
       param = prepare(mesh, E, B)
       prob = ODEProblem(trace!, stateinit, tspan, param)
