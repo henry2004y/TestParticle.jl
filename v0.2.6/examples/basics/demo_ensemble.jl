50ed7894507c4c78f61d1178e2739e49ed9e9b3b
Documenter.jl <documenter@juliadocs.github.io>
2023-09-07 03:08:30 +0000

    build based on 80f67f7

---
+++ v0.2.6/examples/basics/demo_ensemble.jl
@@ -0,0 +1,58 @@
+using JSServe: Page # hide
+Page(exportable=true, offline=true) # hide
+
+using TestParticle
+using OrdinaryDiffEq
+using StaticArrays
+using TestParticleMakie
+using WGLMakie
+
+"Initial state perturbation for EnsembleProblem."
+function prob_func(prob, i, repeat)
+   remake(prob, u0=rand()*prob.u0)
+end
+
+# Initialization
+
+function B(x)
+   return SA[0, 0, 1e-11]
+end
+
+function E(x)
+   return SA[0, 0, 5e-13]
+end
+
+x0 = [0.0, 0.0, 0.0] # initial position, [m]
+u0 = [1.0, 0.0, 0.0] # initial velocity, [m/s]
+stateinit = [x0..., u0...]
+
+param = prepare(E, B, species=Electron)
+tspan = (0.0, 10.0)
+
+trajectories = 10
+
+# Solve for the trajectories
+
+prob = ODEProblem(trace!, stateinit, tspan, param)
+ensemble_prob = EnsembleProblem(prob, prob_func=prob_func)
+sols = solve(ensemble_prob, Tsit5(), EnsembleThreads();
+   trajectories=trajectories, save_idxs=[1,2,3])
+
+# Visualization
+
+f = Figure(fontsize = 18)
+ax = Axis3(f[1, 1],
+   title = "Electron trajectories",
+   xlabel = "X",
+   ylabel = "Y",
+   zlabel = "Z",
+   aspect = :data,
+)
+
+for i in eachindex(sols)
+   lines!(ax, sols[i], label="$i")
+end
+
+f
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
---
+++ v0.2.6/examples/basics/demo_multiple.jl
@@ -0,0 +1,73 @@
+using JSServe: Page # hide
+Page(exportable=true, offline=true) # hide
+
+using TestParticle
+using OrdinaryDiffEq
+using TestParticleMakie
+using WGLMakie
+using Random
+
+# For reproducible results
+Random.seed!(1234)
+
+function trace(x, y, z, E, B; trajectories::Int=10)
+   # Initialize particles
+   x0 = [0.0, 0.0, 0.0] # initial position, [m]
+   u0 = [1.0, 0.0, 0.0] # initial velocity, [m/s]
+   stateinit = [x0..., u0...]
+
+   param = prepare(x, y, z, E, B, species=Electron)
+   tspan = (0.0, 15.0)
+
+   prob = ODEProblem(trace!, stateinit, tspan, param)
+
+   sols = Vector{ODESolution}(undef, trajectories)
+   # Sample from a Maxwellian with bulk speed 0 and thermal speed 1.0
+   vdf = Maxwellian([0.0, 0.0, 0.0], 1.0)
+   v = sample(vdf, trajectories)
+
+   for i in 1:trajectories
+      prob = remake(prob; u0=[x0..., v[:,i]...])
+
+      sol = solve(prob, Tsit5(); save_idxs=[1,2,3])
+      sols[i] = sol
+   end
+
+   return sols
+end
+
+### Initialize grid and field
+
+x = range(-10, 10, length=15)
+y = range(-10, 10, length=20)
+z = range(-10, 10, length=25)
+B = fill(0.0, 3, length(x), length(y), length(z)) # [T]
+E = fill(0.0, 3, length(x), length(y), length(z)) # [V/m]
+
+B[3,:,:,:] .= 1e-11
+E[3,:,:,:] .= 5e-13
+
+trajectories = 10
+
+### Solve for the trajectories
+
+sols = trace(x, y, z, E, B; trajectories)
+
+### Visualization
+
+f = Figure(fontsize = 18)
+ax = Axis3(f[1, 1],
+   title = "Particle trajectories",
+   xlabel = "X",
+   ylabel = "Y",
+   zlabel = "Z",
+   aspect = :data,
+)
+
+for i in eachindex(sols)
+   lines!(ax, sols[i], label="$i")
+end
+
+f
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
