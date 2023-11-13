d3797b680a9aece3a9c6874983b327ac068a43c1
Hongyang Zhou <hyzhou@umich.edu>
2023-11-13 14:44:28 -0500

    Make more extreme comparisons

--- docs/examples/basics/demo_boris.jl
+++ docs/examples/basics/demo_boris.jl
@@ -24,20 +24,20 @@ uniform_E(x) = SA[0.0, 0.0, 0.0]
 x0 = [0.0, 0.0, 0.0]
 v0 = [0.0, 1e5, 0.0]
 stateinit = [x0..., v0...]
-tspan = (0.0, 3e-7)
+tspan = (0.0, 3e-6)
 dt = 3e-11
 param = prepare(uniform_E, uniform_B, species=Electron)
 paramBoris = BorisMethod(param)
 prob = TraceProblem(stateinit, tspan, dt, paramBoris)
 
-traj = trace_trajectory(prob);
-@time traj = trace_trajectory(prob);
+traj = trace_trajectory(prob; savestepinterval=10);
+@time traj = trace_trajectory(prob; savestepinterval=10);
 
 # Now let's compare against other ODE solvers:
 
 prob = ODEProblem(trace!, stateinit, tspan, param)
-sol1 = solve(prob, Tsit5(); adaptive=false, dt, dense=false);
-@time sol1 = solve(prob, Tsit5(); adaptive=false, dt, dense=false);
+sol1 = solve(prob, Tsit5(); adaptive=false, dt, dense=false, saveat=10*dt);
+@time sol1 = solve(prob, Tsit5(); adaptive=false, dt, dense=false, saveat=10*dt);
 
 sol2 = solve(prob, Tsit5());
 @time sol2 = solve(prob, Tsit5());
@@ -52,4 +52,5 @@ lines!(ax, sol2, linestyle=:dot, label="Tsit5 adaptive")
 axislegend(position=:lt, framevisible=false)
 f = DisplayAs.PNG(f) #hide
 
-# We can see that the Boris method is both accurate and fast. When calling OrdinaryDiffEq.jl, we recommend using `Vern9()` as a starting point instead of `Tsit5()`, especially combined with adaptive timestepping.
+# We can see that the Boris method is both accurate and fast; Fixed time step `Tsit5()` is ok, but adaptive `Tsit5()` is pretty bad for long time evolutions.
+# When calling OrdinaryDiffEq.jl, we recommend using `Vern9()` as a starting point instead of `Tsit5()`, especially combined with adaptive timestepping.
