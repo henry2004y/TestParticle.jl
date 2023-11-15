4b3e1f5c51a072d095cc44a4c3751c6712e2973e
Documenter.jl <documenter@juliadocs.github.io>
2023-11-15 03:40:44 +0000

    build based on 360f927

---
+++ v0.4.0/examples/basics/demo_ensemble.jl
@@ -0,0 +1,51 @@
+import DisplayAs #hide
+using TestParticle
+using OrdinaryDiffEq
+using StaticArrays
+using CairoMakie
+CairoMakie.activate!(type = "png")
+
+"Initial state perturbation for EnsembleProblem."
+function prob_func(prob, i, repeat)
+   remake(prob, u0=rand()*prob.u0)
+end
+
+# Initialization
+
+B(x) = SA[0, 0, 1e-11]
+E(x) = SA[0, 0, 5e-13]
+
+x0 = [0.0, 0.0, 0.0] # initial position, [m]
+u0 = [1.0, 0.0, 0.0] # initial velocity, [m/s]
+stateinit = [x0..., u0...]
+
+param = prepare(E, B, species=Electron)
+tspan = (0.0, 10.0)
+
+trajectories = 10
+
+# Solve for the trajectories
+
+prob = ODEProblem(trace!, stateinit, tspan, param)
+ensemble_prob = EnsembleProblem(prob, prob_func=prob_func, safetycopy=false)
+sols = solve(ensemble_prob, Tsit5(), EnsembleThreads();
+   trajectories=trajectories, save_idxs=[1,2,3])
+
+# Visualization
+
+f = Figure(fontsize = 18)
+ax = Axis3(f[1, 1],
+   title = "Electron trajectories",
+   xlabel = "X",
+   ylabel = "Y",
+   zlabel = "Z",
+   aspect = :data,
+)
+
+for i in eachindex(sols)
+   lines!(ax, sols[i], label="$i")
+end
+
+f = DisplayAs.PNG(f) #hide
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
