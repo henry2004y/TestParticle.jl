f01c8c84192310cfd3a13d479e2ef48217d7ccde
Hongyang Zhou <hyzhou@umich.edu>
2023-12-17 12:08:03 -0500

    Improve demo

--- docs/examples/basics/demo_ensemble.jl
+++ docs/examples/basics/demo_ensemble.jl
@@ -20,15 +20,19 @@ using StaticArrays
 using CairoMakie
 CairoMakie.activate!(type = "png")
 
-"Initial state perturbation for EnsembleProblem."
+"Set initial state for EnsembleProblem."
 function prob_func(prob, i, repeat)
-   remake(prob, u0=rand()*prob.u0)
+   # If u0 is immutable (e.g. StaticArrays), use remake; otherwise we can directly modify u0
+   #remake(prob, u0=[i/3, 0.0, 0.0])
+   prob.u0[4] = i / 3 
+
+   prob
 end
 
 ## Initialization
 
 B(x) = SA[0, 0, 1e-11]
-E(x) = SA[0, 0, 5e-13]
+E(x) = SA[0, 0, 1e-13]
 
 x0 = [0.0, 0.0, 0.0] # initial position, [m]
 u0 = [1.0, 0.0, 0.0] # initial velocity, [m/s]
@@ -37,14 +41,13 @@ stateinit = [x0..., u0...]
 param = prepare(E, B, species=Electron)
 tspan = (0.0, 10.0)
 
-trajectories = 10
+trajectories = 3
 
 ## Solve for the trajectories
 
 prob = ODEProblem(trace!, stateinit, tspan, param)
-ensemble_prob = EnsembleProblem(prob, prob_func=prob_func, safetycopy=false)
-sols = solve(ensemble_prob, Tsit5(), EnsembleThreads();
-   trajectories=trajectories, save_idxs=[1,2,3])
+ensemble_prob = EnsembleProblem(prob; prob_func, safetycopy=false)
+sols = solve(ensemble_prob, Tsit5(), EnsembleThreads(); trajectories)
 
 ## Visualization
 
