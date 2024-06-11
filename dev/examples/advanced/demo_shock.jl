b7c002d3bfcc07775c72a67fa4faed02770ef6cd
Documenter.jl <documenter@juliadocs.github.io>
2024-06-11 03:35:09 +0000

    build based on afc9951

--- dev/examples/advanced/demo_shock.jl
+++ dev/examples/advanced/demo_shock.jl
@@ -66,15 +66,16 @@ ensemble_prob = EnsembleProblem(prob; prob_func, safetycopy=false)
 
 sols = solve(ensemble_prob, Vern9(), EnsembleSerial(); trajectories);
 
-function plot_traj(sols; azimuth=1.275pi, elevation=pi/8)
+function plot_traj(sols; azimuth=1.275pi, elevation=pi/8,
+   limits=((-4000.0, 4000.0), (-1000., 1000.), (-2000., 2000.)))
    f = Figure(fontsize=18)
    ax = Axis3(f[1, 1];
-      title = "Particles across MHD shock",
-      xlabel = "x [km]",
-      ylabel = "y [km]",
-      zlabel = "z [km]",
-      aspect = :data,
-      azimuth, elevation,
+      title="Particles across MHD shock",
+      xlabel="x [km]",
+      ylabel="y [km]",
+      zlabel="z [km]",
+      aspect=:data,
+      limits, azimuth, elevation,
    )
 
    invL = 1 / 1e3
