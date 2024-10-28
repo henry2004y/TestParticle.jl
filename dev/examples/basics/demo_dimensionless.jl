57a1d3f9c1bda69d04a97ec25b736a276f17fcc7
Documenter.jl <documenter@juliadocs.github.io>
2024-10-28 21:03:25 +0000

    build based on f1f6197

--- dev/examples/basics/demo_dimensionless.jl
+++ dev/examples/basics/demo_dimensionless.jl
@@ -1,8 +1,7 @@
 import DisplayAs #hide
 using TestParticle
 using TestParticle: qᵢ, mᵢ
-using OrdinaryDiffEq
-
+using OrdinaryDiffEq, StaticArrays
 using CairoMakie
 CairoMakie.activate!(type = "png") #hide
 
@@ -48,7 +47,46 @@ ax = Axis(f[1, 1],
    aspect = DataAspect()
 )
 
-lines!(ax, sol, vars=(1,2))
+lines!(ax, sol, idxs=(1,2))
+
+f = DisplayAs.PNG(f) #hide
+
+param = prepare(xu -> SA[0.0, 0.0, 0.0], xu -> SA[0.0, 0.0, 1.0]; species=User)
+tspan = (0.0, π) # half period
+stateinit = [0.0, 0.0, 0.0, 0.01, 0.0, 0.0]
+prob = ODEProblem(trace_relativistic_normalized!, stateinit, tspan, param)
+sol = solve(prob, Vern9())
+
+### Visualization
+f = Figure(fontsize = 18)
+ax = Axis(f[1, 1],
+   title = "Relativistic particle trajectory",
+   xlabel = "X",
+   ylabel = "Y",
+   #limits = (-0.6, 0.6, -1.1, 0.1),
+   aspect = DataAspect()
+)
+
+lines!(ax, sol, idxs=(1,2))
+
+f = DisplayAs.PNG(f) #hide
+
+param = prepare(xu -> SA[0.0, 0.0, 0.0], xu -> SA[0.0, 0.0, 1.0]; species=User)
+tspan = (0.0, π) # half period
+stateinit = [0.0, 0.0, 0.0, 0.9, 0.0, 0.0]
+prob = ODEProblem(trace_relativistic_normalized!, stateinit, tspan, param)
+sol = solve(prob, Vern9())
+
+### Visualization
+f = Figure(fontsize = 18)
+ax = Axis(f[1, 1],
+   title = "Relativistic particle trajectory",
+   xlabel = "X",
+   ylabel = "Y",
+   aspect = DataAspect()
+)
+
+lines!(ax, sol, idxs=(1,2))
 
 f = DisplayAs.PNG(f) #hide
 
