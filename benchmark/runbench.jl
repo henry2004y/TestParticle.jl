44411ad5113570c50c27bba2d6e52d62ffaf59b6
Hongyang Zhou <hyzhou@umich.edu>
2024-10-30 16:53:13 -0400

    Benchmarking via AirspeedVelocity (#206)
    * Run CI Benchmarks with AirspeedVelocity

--- benchmark/runbench.jl
+++
@@ -1,9 +0,0 @@
-# run this script will generate a benchmark report named report.md
-
-using PkgBenchmark
-
-current = BenchmarkConfig(juliacmd = `julia -O3`)
-baseline = BenchmarkConfig(id = "master", juliacmd = `julia -O3`)
-# result = benchmarkpkg("TestParticle", current; retune=true)
-result = judge("TestParticle", current, baseline; retune=true)
-export_markdown("report.md", result)
