216ab83515e89deba0ac105862cb441861b406f3
Documenter.jl <documenter@juliadocs.github.io>
2025-03-20 19:45:55 +0000

    build based on d2c08c2

--- dev/examples/advanced/demo_cosmicray.jl
+++ dev/examples/advanced/demo_cosmicray.jl
@@ -18,7 +18,7 @@ z = range(-10, 10, length=nz) # [rL0]
 B = fill(0.0, 3, nx, ny, nz) # [B0]
 B[3,:,:,:] .= 1.0
 E(x) = SA[0.0, 0.0, 0.0] # [E₀]
-
+# periodic bc = 2
 param = prepare(x, y, z, E, B; species=User, bc=2)
 
 # Initial condition
