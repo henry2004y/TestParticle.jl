1f82efaab039839478dba4b5f8f6a2a7277a1ce3
Documenter.jl <documenter@juliadocs.github.io>
2025-11-26 14:44:41 +0000

    build based on 09b4f8b

--- dev/examples/advanced/demo_spherical.jl
+++ dev/examples/advanced/demo_spherical.jl
@@ -1,5 +1,5 @@
 import DisplayAs #hide
-import TestParticle
+import TestParticle as TP
 using OrdinaryDiffEq, StaticArrays
 using CairoMakie
 CairoMakie.activate!(type = "png") #hide
@@ -9,26 +9,27 @@ r = logrange(0.1, 10.0, length=4)
 θ = range(0, π, length=8)
 ϕ = range(0, 2π, length=8)
 
-B₀ = 1e-8
+B₀ = 1e-8 # [nT]
 B = zeros(3, length(r), length(θ), length(ϕ))
 
 for (iθ, θ_val) in enumerate(θ)
-    B[1, :, iθ, :] .= B₀ * cos(θ_val)
-    B[2, :, iθ, :] .= -B₀ * sin(θ_val)
+    sinθ, cosθ = sincos(θ_val)
+    B[1, :, iθ, :] .= B₀ * cosθ
+    B[2, :, iθ, :] .= -B₀ * sinθ
 end
 
-B_field = TestParticle.getinterp(TestParticle.SphericalNonUniformR(), B, r, θ, ϕ)
-zero_E = TestParticle.ZeroField()
+B_field = TP.getinterp(TP.SphericalNonUniformR(), B, r, θ, ϕ)
+zero_E = TP.ZeroField()
 
 # Initial condition
-stateinit = let x0 = [3.0, 0.0, -1.0], v0 = [0.0, 1.0, 0.1]
+stateinit = let x0 = [1.0, 0.0, 0.0], v0 = [0.0, 1.0, 0.1]
    [x0..., v0...]
 end
 # Time span
 tspan = (0, 18)
 
-param = TestParticle.prepare(zero_E, B_field, species=TestParticle.Proton)
-prob = ODEProblem(TestParticle.trace!, stateinit, tspan, param)
+param = TP.prepare(zero_E, B_field, species=TP.Proton)
+prob = ODEProblem(TP.trace!, stateinit, tspan, param)
 sol = solve(prob, Vern9())
 
 # Visualization
