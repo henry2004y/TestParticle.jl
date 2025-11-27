3686eba16854dd493977eb8f910e3ddf5fa721ad
Hongyang Zhou <hyzhou@umich.edu>
2025-11-27 15:12:22 -0500

    Inherit vitepress template

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -9,9 +9,9 @@ import {
 } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 
 import VersionPicker from "@/VersionPicker.vue"
-import StarUs from '@/StarUs.vue'
 import AuthorBadge from '@/AuthorBadge.vue'
 import Authors from '@/Authors.vue'
+
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
@@ -23,7 +23,6 @@ export const Theme: ThemeConfig = {
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'nav-bar-content-after': () => [
-        h(StarUs),
         h(NolebaseEnhancedReadabilitiesMenu), // Enhanced Readabilities menu
       ],
       // A enhanced readabilities menu for narrower screens (usually smaller than iPad Mini)
