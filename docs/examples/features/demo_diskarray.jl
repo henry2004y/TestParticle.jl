95f910381ad116b057987a90b1a8850e3dff38c3
Hongyang Zhou <hyzhou@umich.edu>
2025-12-05 16:03:34 -0500

    doc: generate vector field demo data

--- docs/examples/features/demo_diskarray.jl
+++ docs/examples/features/demo_diskarray.jl
@@ -40,13 +40,36 @@ end
 # We create a dummy HDF5 file with some field data.
 
 filename = "example_field.h5"
+
 h5open(filename, "w") do fid
    g = create_group(fid, "mygroup")
    ## Create a dataset with chunking enabled
-   dset = create_dataset(g, "myvector", Float32, (10, 10, 10), chunk = (5, 5, 5))
-   values = [Float32(i + j + k) for i in 1:10, j in 1:10, k in 1:10]
-   write(dset, values)
+   dset_a = create_dataset(g, "A", Float32, (10, 10, 10), chunk = (5, 5, 5))
+   values_a = [Float32(i + j + k) for i in 1:10, j in 1:10, k in 1:10]
+   write(dset_a, values_a)
+   println("Wrote dataset A")
+
+   ## Adding Vector Field B (Bx, By, Bz)
+
+   ## Generate data for the vector components
+   values_bx = [Float32(sin(i) * j) for i in 1:10, j in 1:10, k in 1:10]
+   values_by = [Float32(cos(j) * k) for i in 1:10, j in 1:10, k in 1:10]
+   values_bz = [Float32(i * k / 10) for i in 1:10, j in 1:10, k in 1:10]
+
+   ## Create datasets with the same chunking properties
+   dset_bx = create_dataset(g, "Bx", Float32, (10, 10, 10), chunk = (5, 5, 5))
+   dset_by = create_dataset(g, "By", Float32, (10, 10, 10), chunk = (5, 5, 5))
+   dset_bz = create_dataset(g, "Bz", Float32, (10, 10, 10), chunk = (5, 5, 5))
+
+   ## Write the data
+   write(dset_bx, values_bx)
+   write(dset_by, values_by)
+   write(dset_bz, values_bz)
+
+   println("Wrote datasets Bx, By, Bz")
 end
+
+println("Successfully created $filename")
 
 # ## Interpolation Function
 #
@@ -54,7 +77,7 @@ end
 
 ## Open the file and wrap the dataset
 h5open(filename, "r") do fid
-   da = HDF5DiskArray(fid["mygroup/myvector"])
+   da = HDF5DiskArray(fid["mygroup/A"])
 
    cached = DiskArrays.cache(da)
    itp = extrapolate(
@@ -75,6 +98,8 @@ h5open(filename, "r") do fid
    ## Value at 9.5, 1.0, 1.0. Data is i+j+k.
    println("Expected Periodic: ", 9.5 + 1 + 1)
 end
+
+##TODO: How to interpolate field vectors efficiently?
 
 # ## Cleanup
 # Remove the temporary file.
