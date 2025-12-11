e6cd9cc7018c4219afc8858d56893b88105f44b9
Hongyang Zhou <hyzhou@umich.edu>
2025-12-11 13:15:48 -0500

    doc: update SDA demo

--- docs/examples/applications/demo_shock_drift.jl
+++ docs/examples/applications/demo_shock_drift.jl
@@ -13,7 +13,7 @@ using CairoMakie
 using Random
 CairoMakie.activate!(type = "png") #hide
 
-Random.seed!(42)
+Random.seed!(42);
 
 # ## Shock Parameters
 # We set up a perpendicular shock.
@@ -25,11 +25,11 @@ V₁ = [-545.0, 0.0, 0.0] .* 1e3 # Upstream velocity (flowing left)
 B₁ = [0.0, 0.0, 5.0] .* 1e-9   # Upstream B (z-direction)
 
 V₂ = [-172.0, 0.0, 0.0] .* 1e3 # Downstream velocity
-B₂ = [0.0, 0.0, 15.8] .* 1e-9  # Downstream B
+B₂ = [0.0, 0.0, 15.8] .* 1e-9; # Downstream B
 
 # Calculate Convection Electric Field E = -V x B
 E₁ = cross(B₁, V₁)
-E₂ = cross(B₂, V₂)
+E₂ = cross(B₂, V₂);
 
 # ## Grid Setup
 # We use a 1D grid in x-direction.
@@ -40,10 +40,10 @@ x = range(-x_max, x_max, length=1000) # 4000 km total, 4 km resolution
 B = repeat(B₁, 1, length(x))
 E = repeat(E₁, 1, length(x))
 
-# Set downstream values (x < 0)
+## Set downstream values (x < 0)
 mid_ = findfirst(v -> v >= 0, x)
 B[:, 1:mid_-1] .= B₂
-E[:, 1:mid_-1] .= E₂
+E[:, 1:mid_-1] .= E₂;
 
 # ## E-field Bump
 # Add a Gaussian enhancement to the Electric field at the shock.
@@ -52,7 +52,7 @@ E[:, 1:mid_-1] .= E₂
 bump_amp = 2.0 # Enhance by factor of 2 at peak
 bump_width = 100e3 # 100 km width
 for i in eachindex(x)
-   # Gaussian centered at 0
+   ## Gaussian centered at 0
    factor = 1.0 + bump_amp * exp(-(x[i] / bump_width)^2)
    E[2, i] *= factor
 end
@@ -64,7 +64,7 @@ end
 m_heavy = mᵢ / 5.0
 q_heavy = -qᵢ
 v0_p = V₁
-x0 = 1000e3 # 1000 km upstream
+x0 = 1000e3; # 1000 km upstream
 
 # ### Protons
 
@@ -72,7 +72,7 @@ prob_p = let
    param_p = prepare(x, E, B; species=Proton, bc=3)
    u0_p = [x0, 0.0, 0.0, v0_p...]
    ODEProblem(trace!, u0_p, (0.0, 20.0), param_p)
-end
+end;
 
 # Create ensemble of protons
 function prob_func_p(prob, i, repeat)
@@ -85,17 +85,17 @@ function prob_func_p(prob, i, repeat)
 end
 
 ensemble_p = EnsembleProblem(prob_p; prob_func=prob_func_p)
-sols_p = solve(ensemble_p, Vern9(), EnsembleSerial(); trajectories=10)
+sols_p = solve(ensemble_p, Vern9(), EnsembleSerial(); trajectories=10);
 
 # ### Heavy Electrons
 
 prob_e = let
-   # Create parameter object for Heavy Electron.
+   ## Create parameter object for Heavy Electron.
    param_e = prepare(x, E, B; species=User, bc=3, q=q_heavy, m=m_heavy)
 
    u0_e = [x0, 0.0, 0.0, v0_p...]
    ODEProblem(trace!, u0_e, (0.0, 20.0), param_e)
-end
+end;
 
 function prob_func_e(prob, i, repeat)
    r0 = [x0, (rand()-0.5)*500e3, (rand()-0.5)*500e3]
@@ -105,24 +105,24 @@ function prob_func_e(prob, i, repeat)
 end
 
 ensemble_e = EnsembleProblem(prob_e; prob_func=prob_func_e)
-sols_e = solve(ensemble_e, Vern9(), EnsembleSerial(); trajectories=10)
+sols_e = solve(ensemble_e, Vern9(), EnsembleSerial(); trajectories=10);
 
 # ## Visualization
 
 f = Figure(fontsize = 18)
 ax = Axis3(f[1, 1], title="Shock Drift Acceleration", xlabel="x [km]", ylabel="y [km]", zlabel="z [km]", aspect=:data)
 
-# Plot Protons (Blue)
+## Plot Protons
 for sol in sols_p
    lines!(ax, sol, idxs=(1, 2, 3), color=:blue, linewidth=1.5)
 end
 
-# Plot Heavy Electrons (Yellow/Gold)
+## Plot Heavy Electrons
 for sol in sols_e
    lines!(ax, sol, idxs=(1, 2, 3), color=:gold, linewidth=1.5)
 end
 
-# Draw Shock Plane
+## Draw Shock Plane
 mesh!(ax, Rect3f(Point3f(-10e3, -1000e3, -1000e3), Vec3f(20e3, 2000e3, 2000e3)), color=(:red, 0.3))
 
 f = DisplayAs.PNG(f) #hide
