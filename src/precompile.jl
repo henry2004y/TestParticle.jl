f2f695e71d45a7a09a0ea6e220e32028aa09368f
Hongyang Zhou <hyzhou@umich.edu>
2026-01-26 22:10:44 -0500

    Added get_fields and get_work to precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -39,7 +39,10 @@
         # Adaptive Boris
         alg_adaptive = AdaptiveBoris(dtmax = 1.0)
         sol_adaptive = solve(prob, alg_adaptive)
+
         sol_adaptive_sf = solve(prob, alg_adaptive; save_fields = true, save_work = true)
+        E, B = get_fields(sol_adaptive)
+        work = get_work(sol_adaptive)
 
         # Multistep Boris
         sol_multistep = solve(prob; dt, n = 2)
