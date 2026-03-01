3d7c0b9e685518a7ce02aab5453504319f9f6fd4
Hongyang Zhou <hyzhou@umich.edu>
2026-03-01 10:24:15 -0500

    refactor: substitute Interpolations.jl with FastInterpolations.jl

--- src/TestParticle.jl
+++ src/TestParticle.jl
@@ -1,9 +1,6 @@
 module TestParticle
 
 using LinearAlgebra: norm, ×, ⋅, diag, normalize
-using Interpolations: interpolate, interpolate!, extrapolate, scale, BSpline, Linear,
-    Quadratic, Cubic,
-    Line, OnCell, Periodic, Flat, Gridded
 using SciMLBase: AbstractODEProblem, AbstractODEFunction, AbstractODESolution, ReturnCode,
     BasicEnsembleAlgorithm, EnsembleThreads, EnsembleSerial, EnsembleDistributed,
     DEFAULT_SPECIALIZATION, ODEFunction, ODEProblem, remake,
@@ -46,7 +43,7 @@ export EnsembleSerial, EnsembleThreads, EnsembleDistributed, remake
 
 include("types.jl")
 include("utility/utility.jl")
-include("utility/interpolation.jl")
+include("utility/fastinterpolation.jl")
 include("sampler.jl")
 include("prepare.jl")
 include("gc.jl")
