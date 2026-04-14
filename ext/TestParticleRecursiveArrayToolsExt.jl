81ee0b913bb658d41befb51e8ce3397c5bac9a07
Hongyang Zhou <hyzhou@umich.edu>
2026-04-13 22:30:54 -0400

    Fix get_dv argument ordering

--- ext/TestParticleRecursiveArrayToolsExt.jl
+++ ext/TestParticleRecursiveArrayToolsExt.jl
@@ -8,13 +8,13 @@ using RecursiveArrayTools: ArrayPartition
 function trace!(du, u::ArrayPartition, p, t)
     v = u.x[2]
     du.x[1] .= v
-    du.x[2] .= get_dv(u.x[1], v, p, t)
+    du.x[2] .= get_dv(v, u.x[1], p, t)
     return nothing
 end
 
 function trace(u::ArrayPartition, p, t)
     v = u.x[2]
-    dv = get_dv(u.x[1], v, p, t)
+    dv = get_dv(v, u.x[1], p, t)
     return vcat(v, dv)
 end
 
