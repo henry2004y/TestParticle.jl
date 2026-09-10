dabee487c5133b766026918b4afa7ab00d7c62d4
Hongyang Zhou <hyzhou@umich.edu>
2026-09-10 16:47:04 -0400

    build: Extract the Boris solvers into lib/OrdinaryDiffEqBoris
    Move the Boris solvers from the refactor-OrdinaryDiffEqBoris branch into lib/OrdinaryDiffEqBoris as a standalone SciML-style subpackage built on OrdinaryDiffEqCore, and add a CI job that runs its test suite.
    
    The TestParticle-side refactor that came with the original extraction is dropped: master has since unified the Boris types (#543, #544, #545) and migrated to OrdinaryDiffEq v7 / SciMLBase v3 (#549) independently.
    
    The subpackage is not yet wired into TestParticle as a dependency; this change only makes it a buildable and continuously tested part of the repository.

---
+++ lib/OrdinaryDiffEqBoris/src/boris_caches.jl
@@ -0,0 +1,49 @@
+struct BorisConstantCache <: OrdinaryDiffEqConstantCache end
+
+struct BorisCache{uType, rateType} <: OrdinaryDiffEqMutableCache
+    u::uType
+    uprev::uType
+    tmp::uType
+    k::rateType
+end
+
+function alg_cache(
+        alg::Boris, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
+        ::Val{false}, args...; kwargs...
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    return BorisConstantCache()
+end
+
+function alg_cache(
+        alg::Boris, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
+        ::Val{true}, args...; kwargs...
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    return BorisCache(u, uprev, similar(u), similar(rate_prototype))
+end
+
+struct MultistepBorisConstantCache <: OrdinaryDiffEqConstantCache end
+
+struct MultistepBorisCache{uType, rateType} <: OrdinaryDiffEqMutableCache
+    u::uType
+    uprev::uType
+    tmp::uType
+    k::rateType
+end
+
+function alg_cache(
+        alg::MultistepBoris{N}, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
+        ::Val{false}, args...; kwargs...
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits, N}
+    return MultistepBorisConstantCache()
+end
+
+function alg_cache(
+        alg::MultistepBoris{N}, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
+        ::Val{true}, args...; kwargs...
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits, N}
+    return MultistepBorisCache(u, uprev, similar(u), similar(rate_prototype))
+end
